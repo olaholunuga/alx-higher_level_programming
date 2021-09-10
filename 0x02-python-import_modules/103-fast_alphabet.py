98587f1f7f85822bb5373c4b237b5ceaa0dca220
olaholunuga <olaholunuga@gmail.com>
2021-09-10 16:41:03 -0700

    fast alphabet

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    for c in range(65, 91):
-        print(chr(c), end = "")
-print("")
+import string
+print(string.ascii_uppercase)
