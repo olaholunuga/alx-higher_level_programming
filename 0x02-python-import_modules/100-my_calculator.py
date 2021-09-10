1023e7f78b234d54e293b8f24c6a16e3d55b98b2
olaholunuga <olaholunuga@gmail.com>
2021-09-10 16:49:41 -0700

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
+# 100-my_calculator.py
+
+
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
-	import sys
-	from calculator_1 import add
-	from calculator_1 import sub
-	from calculator_1 import mul
-	from calculator_1 import div
-	argv = [3, '-', 6]
-	if len(argv) != 3:
-		print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-		exit(1)  
-	elif argv[1] != '+' or argv[1] != '-' or argv[1] != '*' or argv[1] != '/':
-		print("Unknown operator. Available operators: +, -, * and /")
-		print(argv[1])
-		exit(1)
-	else:
-		if argv[1] == '+':
-			print("{} + {} = {}\n".format(argv[0], argv[2], add(int(argv[0]), int(argv[2]))))
-		elif argv[1] == '-':
-			print("{} - {} = {}\n".format(argv[0], argv[2], sub(int(argv[0]), int(argv[2]))))
-		elif argv[1] == '*':
-			print("{} * {} = {}\n".format(argv[0], argv[2], mul(int(argv[0]), int(argv[2]))))
-		elif argv[1] == '/':
-			print("{} / {} = {}\n".format(argv[0], argv[2], div(int(argv[0]), int(argv[2]))))
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
