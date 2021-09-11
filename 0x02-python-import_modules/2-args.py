7133978f7925f16e285d3e6cb01ffc25b92949f4
olaholunuga <olaholunuga@gmail.com>
2021-09-10 17:01:46 -0700

    make dynamic script!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
     import sys
-    argv = sys.argv[1:]
-    if len(argv) == 0:
-        print("0 argument.")
-    elif len(argv) == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(len(argv)))
+    i = len(sys.argv) - 1
 
-for i in range(len(argv)):
-    print("{}: {}".format(int(i + 1), argv[i]))
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
