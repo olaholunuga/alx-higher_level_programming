d19fc65a3a8549db51451df49fc5138d024491b0
olaholunuga <olaholunuga@gmail.com>
2021-09-28 17:16:59 -0700

    all done and dusted

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,53 +1,38 @@
 #!/usr/bin/python3
+
+
+
 class Square:
-    """ A class that defines a square by its size
-    """
-    def __eq__(self, other):
-        return self.__size == other.__size
 
-    def __lt__(self, other):
-        return self.__size < other.__size
-
-    def __le__(self, other):
-        return self.__size <= other.__size
-
-    def __ne__(self, other):
-        return self.__size != other.__size
-
-    def __gt__(self, other):
-        return self.__size > other.__size
-
-    def __ge__(self, other):
-        return self.__size >= other.__size
-
-    def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-
-    def area(self):
-        """ Method that returns the square are of the object
-        """
-        return (self.__size ** 2)
+    def __init__(self, __size=0):
+        self.__size = __size
 
     @property
     def size(self):
-        """ Method to returns the size value
-        """
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """ Method to set the size value of the square object
-        """
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("__size must be an integer")
         elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+            raise ValueError("__size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def __eq__(self, other):
+        return (self.__size * self.__size) == (other.__size * other.__size)
+
+    def __gt__(self, other):
+        return (self.__size * self.__size) > (other.__size * other.__size)
+
+    def __ge__(self, other):
+        return (self.__size * self.__size) >= (other.__size * other.__size)
+
+    def __lt__(self, other):
+        return (self.__size * self.__size) < (other.__size * other.__size)
+
+    def __le__(self, other):
+        return (self.__size * self.__size) <= (other.__size * other.__size)
