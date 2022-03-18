f9022c39c610b5d69fa00876f811b8dc406dd9b2
“olaholunuga” <olaholunuga@gmail.com>
2022-03-18 19:53:05 +0100

    all don

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,11 @@
 """Python script that fetches https://alx-intranet.hbtn.io/status
 """
 
-import urllib.request
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
+    import urllib.request
+
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as request:
         content = request.read()
         print("Body response:")
